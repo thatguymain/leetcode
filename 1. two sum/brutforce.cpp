cf9bdc46ae842c6bfd07ab55231118362c2f2b3b
thatguymain <leet.thatguy@inbox.ru>
2024-03-16 19:26:38 +0300

    first

---
+++ 1. two sum/brutforce.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        
+        }
+        return {};
+    }
+};
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    Solution s;
+    vector<int> res = s.twoSum(nums, target);
+    for (int i = 0; i < res.size(); i++) {
+        cout << res[i] << endl;
+    };
+    
+    return 0;
+}
